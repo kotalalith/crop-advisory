488587f89393959c208471ac9e9f3725de129ff6
LalithKota <kotaadityagupta@gmail.com>
2025-09-24 09:58:33 +0530

    Add files via upload

---
+++ horticulture.js
@@ -0,0 +1,29 @@
+window.pages = window.pages || {};
+window.pages['horticulture'] = `
+    <div class="crops-container">
+        <h1>Horticulture Crops</h1>
+        <p>Select a category to explore different horticultural crops.</p>
+        <div class="horticulture-wrapper">
+            <div class="horticulture-container" onclick="showPage('fruits')">
+                <img src="https://cdn-icons-png.flaticon.com/512/415/415744.png" alt="Fruits">
+                <div class="horticulture-text">Fruits</div>
+            </div>
+            <div class="horticulture-container" onclick="showPage('vegetables')">
+                <img src="https://cdn-icons-png.flaticon.com/512/2909/2909834.png" alt="Vegetables">
+                <div class="horticulture-text">Vegetables</div>
+            </div>
+            <div class="horticulture-container" onclick="showPage('flowers')">
+                <img src="https://cdn-icons-png.flaticon.com/512/2529/2529949.png" alt="Flowers">
+                <div class="horticulture-text">Flowers</div>
+            </div>
+            <div class="horticulture-container" onclick="showPage('herbs')">
+                <img src="https://cdn-icons-png.flaticon.com/512/2284/2284236.png" alt="Herbs">
+                <div class="horticulture-text">Herbs</div>
+            </div>
+            <div class="horticulture-container" onclick="showPage('spices')">
+                <img src="https://cdn-icons-png.flaticon.com/512/2544/2544036.png" alt="Spices">
+                <div class="horticulture-text">Spices</div>
+            </div>
+        </div>
+    </div>
+`;
